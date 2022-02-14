c79ad9ff645f4c1260c52921e9959a2fca6e3c2e
Tate Frost <tatefrost96@gmail.com>
2022-02-14 13:00:26 -0700

    created DB model

--- model.py
+++ model.py
@@ -1,8 +1,56 @@
 """Models and database functions for Stalk-Star"""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import PrimaryKeyConstraint
 
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    """User of Stalk-Star website."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"<User user_id={self.user_id} email={self.email}>"
+
+
+class Artist(db.Model):
+    """An artist on Youtube"""
+
+    __tablename__ = "artists"
+
+    artist_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    artist_name = db.Column(db.String(64), nullable=False)
+    artist_previous_song = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+
+        return f"<Artist artist_id={self.artist_id} name={self.artist_name} previous_song={self.artist_previous_song}>"
+
+
+class Follows(db.Model):
+    """A user to artist following relationship"""
+
+    __tablename__ = "artist_followers"
+
+    following_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.artist_id'))
+
+    user = db.relationship("User", backref=db.backref("stalk-star", order_by=artist_id))
+    
+    artist = db.relationship("Artist", backref=db.backref("stalk-star", order_by=artist_id))
+
+    def __repr__(self):
+
+        return f"<followings following_id={self.following_id} user_id={self.user_id} artist_id={self.artist_id}>"
 
 
 ##############################################################################
