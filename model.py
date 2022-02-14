85700f107dfd442499ad832d74a2f30b3a196265
Tate Frost <tatefrost96@gmail.com>
2022-02-14 14:40:27 -0700

    fixed DB URI

--- model.py
+++ model.py
@@ -60,7 +60,7 @@ def connect_to_db(app):
     """Connect the database to our Flask app."""
 
     # Configure to use our PstgreSQL database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:FreightTost96@localhost/stalk-star'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:secretlocalhost/stalk-star'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
