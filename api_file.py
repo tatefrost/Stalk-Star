eaac2b43a05738f8420dee4f0fb9d10f35c98b64
Tate Frost <tatefrost96@gmail.com>
2022-02-17 10:20:17 -0700

    built functions to search for artists and parse the search results for information

--- api_file.py
+++ api_file.py
@@ -7,17 +7,58 @@ ytmusic = YTMusic("headers_auth.json")
 
 
 # Get artist name and channel id and check if they are in the database, if not it will add them
-
 def search_artist(search_term):
-    ytmusic.search(search_term)
+    """Search artist on Youtube music, get search results in list"""
+
+    return ytmusic.search(search_term, filter='artists', limit=2)
 
 
-def parse_name_id(artist):
-    pass
+def parse_name_id(results):
+        """Parse artist name and ID out from search results"""
+
+        # Split search results from artist search
+        split_search_results = results.split(",")
+
+        # Get lines that artist name and ID are on 
+        artist_name_line = split_search_results[2]
+        artist_id_line = split_search_results[5]
+
+        # Gets artist name and ID from their lines 
+        artist_name_split = artist_name_line.split(":")
+        artist_id_split = artist_id_line.split(":")
+
+        # Instantiates artist name and ID values, and removes quotation marks around values
+        artist_name = artist_name_split[1].strip("'")[2:]
+        artist_id = artist_id_split[1].strip("'")[2:]
+
+        return artist_name, artist_id
+
+                
+
+
+def latest_song(artist):
+        """Search for an artists most previous release and parse out the title"""
+
+        # Search youtube music for "Artist" latest song
+        search_result = ytmusic.search(f"{artist} latest song")
+
+        # Split search result
+        split = str(search_result).split(",")
+
+        # Get line that video title is on
+        title_line = split[2]
+
+        # Parse for song title
+        title = title_line.split(":")[1][1:]
+
+        return title
 
 
 def check_db(id):
-    pass
+        """Check if the artist is in the database"""
+
+    
+        pass
 
 # Check every half hour for alterations, generate reminder if there are any and send to any user whom follows that artist id in the database 
 
@@ -27,10 +68,9 @@ def check_db(id):
 if __name__ == "__main__":
 
 
-    # V V V test functions V V V
-    search_artist = ytmusic.search("Billie Eillish")
-    search_results = ytmusic.get_artist('UCERrDZ8oN0U_n9MphMKERcg')
-    post = ytmusic.get_artist_albums('UCeLHszkByNZtPKcaVXOCOQQ','6gPaAUNxQUJDb0FCQ25FQUFHVnVBQUZWVXdBQlZWTUFBUUJHUlcxMWMybGpYMlJsZEdGcGJGOWhjblJwYzNRQUFRQUJRd0FBQVFBQkFBQUJBUVFCb2xYSUNMd0RHaGhWUTNsRU0xaFhVa3M1YTI4dGFYcG1NbTVDVTBacGRIZUNBUmhWUTNsRU0xaFhVa3M1YTI4dGFYcG1NbTVDVTBacGRIY0FBUkMtMHFXXzA0TDJBaG9DY0drWUFDb1BZWEowYVhOMFgzSmxiR1ZoYzJWek1MSFUwT1dYOGNueWNB')
-    albums = ytmusic.get_artist_albums('UCERrDZ8oN0U_n9MphMKERcg', '6gPaAUNxQUJDb0FCQ25FQUFHVnVBQUZWVXdBQlZWTUFBUUJHUlcxMWMybGpYMlJsZEdGcGJGOWhjblJwYzNRQUFRQUJRd0FBQVFBQkFBQUJBUVFCb2xwNkNMd0RHaGhWUTBWU2NrUmFPRzlPTUZWZmJqbE5jR2hOUzBWU1kyZUNBUmhWUTBWU2NrUmFPRzlPTUZWZmJqbE5jR2hOUzBWU1kyY0FBUkNxNFktTDRJVDJBaG9DY0drWUFDb1BZWEowYVhOMFgzSmxiR1ZoYzJWek1MSFUwT1dYOGNueWNB')
-
-    print(post)
+        # V V V test functions V V V
+        artist = search_artist("Guns n' roses")
+        parse = parse_name_id(str(artist))
+        latest = latest_song(str(parse).split(",")[0][1:])
+        print(latest)
+    
